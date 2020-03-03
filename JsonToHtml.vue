18ebb53fce9231ba4f0055b1cdfb5cbf5e8f11ea
VTOMV <45344300+VTOMV@users.noreply.github.com>
2020-03-03 16:57:29 +0800

    Create JsonToHtml.vue

---
+++ JsonToHtml.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="table-main">
+    <div>
+      <textarea v-model="json" style="width:100%;height:400px;overflow:scroll"></textarea>
+      <button @click="jsonFormat()">提交</button>
+      <VueJsonTable :data="data"></VueJsonTable>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueJsonTable from "./vue-table/VueJsonTable";
+
+export default {
+  name: "JsonToHtml",
+  components: {
+    VueJsonTable
+  },
+  data() {
+    return {
+      data: new Object(),
+      json: ""
+    };
+  },
+  methods: {
+    jsonFormat() {
+      this.data = new Object();
+    }
+  }
+};
+</script>
+
